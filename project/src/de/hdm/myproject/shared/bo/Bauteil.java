3f63c1779eba4cd1c9e9fc892c05b4c7354e4072
IvanKurtovic <Ivan@Ivan-PC>
2015-05-07 13:58:00 +0200

    neue Klasse Nutzer Mapper neue Klasse

--- project/src/de/hdm/myproject/shared/bo/Bauteil.java
+++ project/src/de/hdm/myproject/shared/bo/Bauteil.java
@@ -7,4 +7,16 @@ public class Bauteil extends BusinessObject {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	
+
+	public void setNutzerID(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setId(int int1) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
