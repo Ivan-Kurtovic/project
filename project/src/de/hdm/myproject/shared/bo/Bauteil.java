749290b1e5357d8991e73d1fde2987da031b6f98
IvanKurtovic <Ivan@Ivan-PC>
2015-05-15 10:42:02 +0200

    Anlegen der Klasse Bauteil Anlegen der Attribute bauteilBeschreibung, materialBeschreibung sowie der Getter und Setter Methoden für die jeweiligen Members

--- project/src/de/hdm/myproject/shared/bo/Bauteil.java
+++ project/src/de/hdm/myproject/shared/bo/Bauteil.java
@@ -2,13 +2,48 @@ package de.hdm.myproject.shared.bo;
 
 public class Bauteil extends BusinessObject {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
+	private String bauteilBeschreibung = "testtext";
+	private String materialBeschreibung = "testtext";
 
 	
+	
+	
+	
+	public String getBauteilBeschreibung() {
+		return this.bauteilBeschreibung;
+	}
+	
+	
+	public void setBauteilBeschreibung(String bauteilBeschreibung) {
+		this.bauteilBeschreibung = bauteilBeschreibung;
+	}
 
 	
-
+	
+	public String getMaterialBeschreibung() {
+		return this.materialBeschreibung;
+	}
+	
+	
+	public void setMaterialBeschreibung(String materialBeschreibung) {
+		this.materialBeschreibung = materialBeschreibung;
+	}
+	
+	
+	
+	/**
+	   * Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz.
+	   * Diese besteht aus dem Text, der durch die <code>toString()</code>-Methode
+	   * der Superklasse erzeugt wird, ergänzt durch den Name  und Teilbeschreibung des 
+	   * jeweiligen Bauteils.
+	   */
+	@Override
+	public String toString() {
+	    return super.toString() + " Teilbeschreibung: " + this.bauteilBeschreibung
+	    		+"Materialbeschreibung"+this.materialBeschreibung;
+	  }
 }
+
+
