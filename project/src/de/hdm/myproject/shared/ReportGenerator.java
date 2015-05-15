46f32e294a806e60c97a6f6545af56cb511e42ad
IvanKurtovic <Ivan@Ivan-PC>
2015-05-15 18:30:43 +0200

    Gelöschte Interfaces wieder ins Remote Repository gepuscht

---
+++ project/src/de/hdm/myproject/shared/ReportGenerator.java
@@ -0,0 +1,11 @@
+package de.hdm.myproject.shared;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+
+
+
+public interface ReportGenerator extends RemoteService {
+
+	void init();
+
+}
---
+++ project/src/de/hdm/myproject/shared/ReportGeneratorAsync.java
@@ -0,0 +1,14 @@
+package de.hdm.myproject.shared;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+
+public interface ReportGeneratorAsync {
+
+	void init(AsyncCallback<Void> initReportGeneratorCallback);
+
+	
+		
+	}
+
+
