13ea143c0f40f9525dd8c8e095c92472e98010ca
IvanKurtovic <Ivan@Ivan-PC>
2015-05-15 10:26:16 +0200

    Anlegen der Create Methode im Synchronen und im Asynchronen Interface und in der Impl Klasse

--- project/src/de/hdm/myproject/shared/StuecklistenAdministration.java
+++ project/src/de/hdm/myproject/shared/StuecklistenAdministration.java
@@ -2,6 +2,11 @@ package de.hdm.myproject.shared;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 
+import de.hdm.myproject.shared.bo.Bauteil;
+
 public interface StuecklistenAdministration extends RemoteService {
+	
+ 	Bauteil createBauteil(String name, String bauteilBeschreibung,
+			String materialBeschreibung) throws IllegalArgumentException;
 
 }
--- project/src/de/hdm/myproject/shared/StuecklistenAdministrationAsync.java
+++ project/src/de/hdm/myproject/shared/StuecklistenAdministrationAsync.java
@@ -1,5 +1,12 @@
 package de.hdm.myproject.shared;
 
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+import de.hdm.myproject.shared.bo.Bauteil;
+
 public interface StuecklistenAdministrationAsync {
+
+	void createBauteil(String name, String bauteilBeschreibung,
+			String materialBeschreibung, AsyncCallback<Bauteil> callback);
 
 }
